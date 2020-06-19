981f936243af0da81b14d337d2dc7f257af42646
Nicholas Reynolds <nkreynolds@ucdavis.edu>
2020-06-18 19:21:32 -0700

    Display mock data

--- src/server.py
+++ src/server.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from os import path
+from flask import Flask, render_template, json
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Displays json data to client
+@app.route('/data', methods=['GET'])
+def data():
+    return render_template('disp_json.jinja', data=load_data())
+
+
+# Loads json data from mock file. Can be retrofitted later to query database
+def load_data():
+    filename = path.join(app.root_path, 'MOCK_DATA.json')
+    fp = open(filename)
+    return json.load(fp)
